c01d5e17425dd8f0b1327dd66544f016fb767ba4
ibrahem khaled <xx.vvv48@gmail.com>
2025-12-05 13:46:35 +0200

    change location icon from home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -557,9 +557,11 @@
                                                 class="px-2 py-1.5 md:px-3 md:py-2 whitespace-nowrap text-xs font-medium text-right">
                                                 @if ($council->google_map_url)
                                                     <a href="{{ $council->google_map_url }}" target="_blank"
-                                                        class="text-blue-600 hover:text-blue-900 transition-colors"
+                                                        class="text-blue-600 hover:text-blue-900 transition-colors inline-flex items-center"
                                                         title="فتح في جوجل ماب" onclick="event.stopPropagation();">
-                                                        <i class="fas fa-map-marked-alt text-xs"></i>
+                                                        <svg class="w-4 h-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                            <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" fill="#EA4335" stroke="#FFFFFF" stroke-width="1.5"/>
+                                                        </svg>
                                                         <span class="mr-1 hidden lg:inline text-xs">الخريطة</span>
                                                     </a>
                                                 @else
@@ -678,7 +680,9 @@
                                                                 class="inline-flex items-center justify-center text-blue-600 hover:text-blue-800 group cursor-pointer transition-colors"
                                                                 onclick="event.stopPropagation();"
                                                                 title="{{ $event->location_name ?? 'فتح الموقع على الخريطة' }}">
-                                                                <i class="fas fa-map-marked-alt text-xs group-hover:scale-110 transition-transform"></i>
+                                                                <svg class="w-4 h-4 group-hover:scale-110 transition-transform" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                    <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" fill="#EA4335" stroke="#FFFFFF" stroke-width="1.5"/>
+                                                                </svg>
                                                             </a>
 
                                                         </div>
