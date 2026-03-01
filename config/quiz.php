a98b0d5eea5ad35f8990e629872677bce147cb37
ibrahem khaled <xx.vvv48@gmail.com>
2026-03-01 17:19:22 +0200

    return question time 60 sec

--- config/quiz.php
+++ config/quiz.php
@@ -9,6 +9,6 @@ return [
     | Number of seconds after competition start_at before questions are shown
     | to visitors. Until then, a countdown is displayed.
     */
-    'question_visible_after_seconds' => (int) env('QUIZ_QUESTION_VISIBLE_AFTER_SECONDS', 300),
+    'question_visible_after_seconds' => (int) env('QUIZ_QUESTION_VISIBLE_AFTER_SECONDS', 60),
 
 ];
